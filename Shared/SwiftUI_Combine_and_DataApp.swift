8dc4d9f9b362b7000d1901d584069efb010d1205
Ashraful Islam Rupu <rupuislamashraful7@gmail.com>
2022-04-26 20:53:53 +0600

    Initia Commit

---
+++ Shared/SwiftUI_Combine_and_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_and_DataApp.swift
+//  Shared
+//
+//  Created by Ashraful Islam Rupu on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_and_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
